743c3449dd872363cec9c5c71a6282d21762e969
Tijmen Roes <t.roes@student.fontys.nl>
2018-05-20 23:11:45 +0200

    Test uitgevoerd zodat je kan welke letter het is
    Dit is voor later, ook ben ik overgestapt op php zodat ik evt. de file die de letters uitleest kan includen.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.php');
 });
 
 io.on('connection', function(socket){
