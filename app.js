79dabdead2c6c125638a19fd3ee80f5804c67d35
Tijmen Roes <t.roes@student.fontys.nl>
2018-06-15 12:49:20 +0200

    Arduino link nieuwe pogingen.
    Lukte niet tho

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 
-var five = require("johnny-five");
 
-//var board = new five.Board();
-
-
-var temporal = require("temporal");
-
-var morse = require('morse');
 
 //mogelijk iets om te doen. Decode het op app.js en gooi alles in 1 string. Daarna ga je met een loop de string af per letter en waar een . of - is zal die iets moeten uitvoeren.
 //daarna word een timer doorgestuurd naar de index
@@ -29,36 +22,50 @@ http.listen(3000, function(){ //wannneer de server start
 
  io.on('connection', function(socket){
     socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
+        console.log('Morse:: ' + msg);
     });
 });
 
 
 
 //hier begint de arduino code
-/*
-board.on("ready", function() {
-    var led = new five.Led(13);
-    io.on('connection', function (socket) {
-        socket.on('chat message', function (msg) {
 
-            led.toggle();
-            temporal.delay(500, function() {
-                led.toggle()
-            });
-              temporal.delay(1000, function() {
-                led.toggle()
-            });
-            temporal.delay(1500, function() {
-                led.toggle()
-            });
-            temporal.delay(2000, function() {
-                led.toggle()
-            });
-            temporal.delay(2500, function() {
-                led.toggle()
-            });
-            });
+var five = require("johnny-five");
+
+var board = new five.Board();
+
+board.on("ready", function() {
+    var led = new five.Led(11);
+    io.on('connection', function (socket) {
+        socket.on('chat message', function () {
+
+            function myLoop () {           //  create a loop function
+
+                const counter = fullMorse[d].val * 1000;
+                setTimeout(function () {    //  call a  setTimeout when the loop is called
+                    d++;       //  your code here
+
+                    haha = fullMorse[d].on + ": " + fullMorse[d].val;
+                    document.getElementById("demo").innerHTML = haha;
+                    if (fullMorse[d].on === true) {
+                        $('.lol').css("opacity", "1");
+
+
+                    } else {
+                        $('.lol').css("opacity", "0");
+                    }
+
+                    //  increment the counter
+
+                    if (d < fullMorse.length -1  ) {            //  als d nog niet klaar is, start de loop weer.
+                        myLoop();             //  ..  again which will trigger another
+                    }                        //  ..  setTimeout()
+                }, counter)
+            }
+
+            myLoop();                      //  start the loop
+
         })
-    });
-*/
+    })
+});
+
--- johnny.js
+++ johnny.js
@@ -10,47 +10,78 @@ var board = new five.Board();
 board.on("ready", function() {
     var led = new five.Led(11);
 
-    var poepje = [
+    var fullMorse = [
+        {on: false, val: 1} ,
         {on: true, val: 3} ,
         {on: false, val: 1},
         {on: true, val: 1},
         {on: false, val: 1}
     ];
-
-     for ( i = 0; i < poepje.length - 1; i++)
-    {
+    console.log(fullMorse);
 
 
-        temporal.delay(1000, function() {
-            var value = poepje[i].val;
-            GetChar(poepje[i].val);
-        })
+
+    function myLoop () {           //  create a loop function
+
+        const counter = fullMorse[d].val * 1000;
+        setTimeout(function () {    //  call a  setTimeout when the loop is called
+            d++;       //  your code here
+
+            haha = fullMorse[d].on + ": " + fullMorse[d].val;
+            document.getElementById("demo").innerHTML = haha;
+            if (fullMorse[d].on === true) {
+                $('.lol').css("opacity", "1");
+
+
+            } else {
+                $('.lol').css("opacity", "0");
+            }
+
+            //  increment the counter
+
+            if (d < fullMorse.length -1  ) {            //  als d nog niet klaar is, start de loop weer.
+                myLoop();             //  ..  again which will trigger another
+            }                        //  ..  setTimeout()
+        }, counter)
     }
 
-function GetChar(value) {
+    myLoop();                      //  start the loop
+    /*
+         for ( i = 0; i < poepje.length - 1; i++)
         {
-            switch (value) {
-                case 3:
-                    console.log("3");
 
-                    break;
-                case 1:
-                    console.log("11111");
-                    led.on();
-                    temporal.delay(500, function() {
 
-                       led.off();
-
-                    });
-
-                    break;
-                default:
-                    console.log("def");
-                    led.off(500);
-                    break;
-            }
+            temporal.delay(1000, function() {
+                var value = poepje[i].val;
+                GetChar(poepje[i].val);
+            })
         }
 
-    }
+    function GetChar(value) {
+            {
+                switch (value) {
+                    case 3:
+                        console.log("3");
+
+                        break;
+                    case 1:
+                        console.log("11111");
+                        led.on();
+                        temporal.delay(500, function() {
+
+                           led.off();
+
+                        });
+
+                        break;
+                    default:
+                        console.log("def");
+                        led.off(500);
+                        break;
+                }
+            }
+
+        }
+        */
 });
 
