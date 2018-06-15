87874fd7f79ba450c6dc5666feabfb6876e5bf2f
Tijmen Roes <t.roes@student.fontys.nl>
2018-06-15 15:12:38 +0200

    Arduino koppeling met webpage gelukt!
    vanuit de webserver wordt nu de socket verstuurd met de delay van de code die in de webserver staat.

--- app.js
+++ app.js
@@ -4,12 +4,15 @@
 //mogelijk iets om te doen. Decode het op app.js en gooi alles in 1 string. Daarna ga je met een loop de string af per letter en waar een . of - is zal die iets moeten uitvoeren.
 //daarna word een timer doorgestuurd naar de index
 
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
+    app.use(express.static(__dirname + '/img'));
+
 });
 
 io.on('connection', function(socket){ //als er een connectie gemaakt word
@@ -22,7 +25,7 @@ http.listen(3000, function(){ //wannneer de server start
 
  io.on('connection', function(socket){
     socket.on('chat message', function(msg){
-        console.log('Morse:: ' + msg);
+    //    console.log('Morse:: ' + msg);
     });
 });
 
@@ -37,33 +40,20 @@ var board = new five.Board();
 board.on("ready", function() {
     var led = new five.Led(11);
     io.on('connection', function (socket) {
-        socket.on('chat message', function () {
+        socket.on('chat message', function (msg) {
 
-            function myLoop () {           //  create a loop function
-
-                const counter = fullMorse[d].val * 1000;
-                setTimeout(function () {    //  call a  setTimeout when the loop is called
-                    d++;       //  your code here
-
-                    haha = fullMorse[d].on + ": " + fullMorse[d].val;
-                    document.getElementById("demo").innerHTML = haha;
-                    if (fullMorse[d].on === true) {
-                        $('.lol').css("opacity", "1");
-
-
-                    } else {
-                        $('.lol').css("opacity", "0");
-                    }
-
-                    //  increment the counter
-
-                    if (d < fullMorse.length -1  ) {            //  als d nog niet klaar is, start de loop weer.
-                        myLoop();             //  ..  again which will trigger another
-                    }                        //  ..  setTimeout()
-                }, counter)
-            }
-
-            myLoop();                      //  start the loop
+           console.log(msg);
+           if(msg == "on"){
+               console.log("STAAT AAN MAN");
+               led.on();
+           }
+           else if (msg== "off"){
+               console.log("STAAT UIT MAN");
+               led.off();
+           }
+           else {
+               console.log("lol");
+           }
 
         })
     })
