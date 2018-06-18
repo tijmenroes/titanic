0edf85019dfadc19d9150f3b878abd9f0368709b
Tijmen Roes <t.roes@student.fontys.nl>
2018-06-18 12:20:55 +0200

    Piezo werkend

--- piezo.js
+++ piezo.js
@@ -11,14 +11,11 @@ board.on("ready", function() {
     });
 
     // Plays a song
+    piezo.play({
+        song: [
+            [ 698, 20 ], // Play frequency 698 for 1 beat
+            [ 831, 2 ] // ...
+        ]
+    });
 
-    // Plays the same song with a string representation
-    piezo.play(five.Piezo.Notes.d5, 5000);
-}); // Play note d5 for 1 second
-        // song is composed by a string of notes
-        // a default beat is set, and the default octave is used
-        // any invalid note is read as "no note"
-
-
-
-
+});
